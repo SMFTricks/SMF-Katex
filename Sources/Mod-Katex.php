7cd5493d9c2bbe8eaeb08b03875efcf529a3df4b
Diego Andrés <diegoandres_cortes@outlook.com>
2021-06-07 13:23:01 -0500

    update to new version
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Sources/Mod-Katex.php
+++ Sources/Mod-Katex.php
@@ -2,7 +2,7 @@
 
 /**
  * @package Katex MOD
- * @version 1.0
+ * @version 1.0.1
  * @author Diego Andrés <diegoandres_cortes@outlook.com>
  * @copyright Copyright (c) 2020, SMF Tricks
  * @license https://www.gnu.org/licenses/gpl-3.0.en.html
@@ -15,9 +15,9 @@ class Katex
 {
 	public static function load_customs()
 	{
-		loadCSSFile('https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css', array('external' => true, 'minimize' => false, 'attributes' => ['integrity' => 'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X', 'crossorigin' => 'anonymous']));
-		loadJavaScriptFile('https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js', array('external' => true, 'minimize' => false, 'attributes' => ['integrity' => 'sha384-g7c+Jr9ZivxKLnZTDUhnkOnsh30B4H0rpLUpJ4jAIKs4fnJI+sEnkvrMWph2EDg4', 'crossorigin' => 'anonymous'], 'defer' => true));
-		loadJavaScriptFile('https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.min.js', array('external' => true, 'minimize' => false, 'attributes' => ['integrity' => 'sha384-mll67QQFJfxn0IYznZYonOWZ644AWYC+Pt2cHqMaRhXVrursRwvLnLaebdGIlYNa', 'crossorigin' => 'anonymous', 'onload' => 'renderMathInElement(document.body);'], 'defer' => true));
+		loadCSSFile('https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css', array('external' => true, 'minimize' => false, 'attributes' => ['integrity' => 'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X', 'crossorigin' => 'anonymous']));
+		loadJavaScriptFile('https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.js', array('external' => true, 'minimize' => false, 'attributes' => ['integrity' => 'sha384-g7c+Jr9ZivxKLnZTDUhnkOnsh30B4H0rpLUpJ4jAIKs4fnJI+sEnkvrMWph2EDg4', 'crossorigin' => 'anonymous'], 'defer' => true));
+		loadJavaScriptFile('https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/contrib/auto-render.min.js', array('external' => true, 'minimize' => false, 'attributes' => ['integrity' => 'sha384-mll67QQFJfxn0IYznZYonOWZ644AWYC+Pt2cHqMaRhXVrursRwvLnLaebdGIlYNa', 'crossorigin' => 'anonymous', 'onload' => 'renderMathInElement(document.body);'], 'defer' => true));
 	}
 
 	public static function bbc_buttons(&$bbc_tags, &$editor_tag_map)
